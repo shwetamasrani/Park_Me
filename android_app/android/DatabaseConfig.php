ef31791d7168db290d7a47fad1e72b337f2f2d3c
shwetamasrani <shwetamasrani19@gmail.com>
2018-10-19 19:25:05 +0530

    Add files via upload

---
+++ android_app/android/DatabaseConfig.php
@@ -0,0 +1,15 @@
+<?php
+
+//Define your host here.
+$HostName = "localhost";
+
+//Define your database username here.
+$HostUser = "id4493440_cityp";
+
+//Define your database password here.
+$HostPass = "Sarthak16";
+
+//Define your database name here.
+$DatabaseName = "id4493440_citypark";
+
+?>
